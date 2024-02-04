34055db33cebaf9512f264ce40e8108e0e6e4ac5
5ldkamal <39638396+5ldkamal@users.noreply.github.com>
2024-02-04 13:30:51 +0200

    Create Longest Nice Substring.swift

---
+++ DivideAndConqure/Longest Nice Substring.swift
@@ -0,0 +1,48 @@
+//
+//  Longest Nice Substring.swift
+//  LeetCode
+//
+//  Created by Khaled Kamal on 29/01/2024.
+//
+
+import Foundation
+// Longest Nice Substring
+func longestNiceSubstring(_ s: String) -> String {
+    if s.count < 2 {
+        return ""
+    }
+    var subString = ""
+    
+    var left = s.startIndex
+    let right = s.endIndex
+    
+    while left < right {
+        var iLeft = s.index(after: left)
+        
+        while iLeft < right {
+            let _subString = String(s[left ... iLeft])
+            
+            if isNice(_subString), _subString.count > subString.count {
+                subString = _subString
+            }
+            
+            iLeft = s.index(after: iLeft)
+        }
+        
+        left = s.index(after: left)
+    }
+    
+    func isNice(_ sub: String) -> Bool {
+        return sub.allSatisfy { c in sub.contains(String(c.lowercased())) && sub.contains(String(c.uppercased())) }
+    }
+    
+    return subString
+}
+
+func testLongestNiceSubstring() {
+    print(longestNiceSubstring("YazaAay"))
+    
+    print(longestNiceSubstring("Bb"))
+    
+    print(longestNiceSubstring("c"))
+}
